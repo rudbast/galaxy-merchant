898f06cb8b007121a08f54586591800c98e8c690
Rudy <rudolf_bast@live.com>
2019-02-21 16:07:38 +0700

    Add implementation of roman parser

--- roman/parser.go
+++ roman/parser.go
@@ -1,5 +1,52 @@
 package roman
 
+import (
+	"errors"
+)
+
+var (
+	ConstantValueMap = map[rune]int64{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+)
+
+// Parse a given roman number text into decimal. Returns error if a non roman numeral character found.
 func Parse(number string) (int64, error) {
-	return 0, nil
+	var lastChar rune
+	var result int64 = 0
+
+	for _, num := range number {
+		value, ok := ConstantValueMap[num]
+		if !ok {
+			return 0, errors.New("roman: invalid roman character found")
+		}
+
+		switch num {
+		case 'V', 'X':
+			if lastChar == 'I' {
+				value -= 2 * ConstantValueMap['I']
+			}
+
+		case 'L', 'C':
+			if lastChar == 'X' {
+				value -= 2 * ConstantValueMap['X']
+			}
+
+		case 'D', 'M':
+			if lastChar == 'C' {
+				value -= 2 * ConstantValueMap['C']
+			}
+		}
+
+		result += value
+		lastChar = num
+	}
+
+	return result, nil
 }
