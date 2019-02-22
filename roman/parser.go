a4fec98b34c50b31413dc14a96f84f7e4eb99f9f
Rudy <rudolf_bast@live.com>
2019-02-22 17:13:26 +0700

    Add validation on character occurrence

--- roman/parser.go
+++ roman/parser.go
@@ -14,11 +14,14 @@ var (
 		'D': 500,
 		'M': 1000,
 	}
+
+	ErrCharOcurrenceLimit = errors.New("roman: character ocurrence exceeds limit")
 )
 
 // Parse a given roman number text into decimal. Returns error if a non roman numeral character found.
 func Parse(number string) (int64, error) {
 	var lastChar rune
+	var charOccurrence int = 1
 	var result int64 = 0
 
 	for _, num := range number {
@@ -27,6 +30,7 @@ func Parse(number string) (int64, error) {
 			return 0, errors.New("roman: invalid roman character found")
 		}
 
+		// Re-calculate for subtraction implied by previous character.
 		switch num {
 		case 'V', 'X':
 			if lastChar == 'I' {
@@ -42,6 +46,21 @@ func Parse(number string) (int64, error) {
 			if lastChar == 'C' {
 				value -= 2 * ConstantValueMap['C']
 			}
+		}
+
+		// Validate subsequent occurences on certain character.
+		if num == lastChar {
+			charOccurrence++
+
+			if charOccurrence > 3 {
+				switch num {
+				case 'I', 'X', 'C', 'M':
+					return 0, ErrCharOcurrenceLimit
+				}
+			}
+		} else {
+			// Reset counter on character change.
+			charOccurrence = 1
 		}
 
 		result += value
--- roman/parser_test.go
+++ roman/parser_test.go
@@ -12,6 +12,10 @@ func TestParse(t *testing.T) {
 		testCase{"MMVI", 2006},
 		testCase{"MCMXLIV", 1944},
 		testCase{"MCMIII", 1903},
+		testCase{"MMIII", 2003},
+		testCase{"LXXXIX", 89},
+		testCase{"DCCCXC", 890},
+		testCase{"MMMCM", 3900},
 	}
 
 	for _, c := range cases {
@@ -23,6 +27,23 @@ func TestParse(t *testing.T) {
 
 		if val != c.Output {
 			t.Errorf("Parse failure, input: %s, expected: %d, got: %d.", c.Input, c.Output, val)
+			return
+		}
+	}
+}
+
+func TestParseOccurenceExceeded(t *testing.T) {
+	cases := []string{
+		"IIII",
+		"MCCCCX",
+		"MDCLLXXXX",
+		"MMMMCM",
+	}
+
+	for _, c := range cases {
+		_, err := Parse(c)
+		if err != ErrCharOcurrenceLimit {
+			t.Errorf("Parse failure, input: %s, expected error: %s, got: %s.", ErrCharOcurrenceLimit, err, c)
 			return
 		}
 	}
